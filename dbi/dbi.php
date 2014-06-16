015ac40781197b153d5dafeaecb4d12066b850a3
Jonathan Lawton <jonathan.lawton@chacha.com>
2014-06-16 03:28:37 -0400

    Update dbi.php

--- dbi/dbi.php
+++ dbi/dbi.php
@@ -8,9 +8,8 @@
 		protected $connection = array();
 		
 		// Query strings
-		public $query;
 		public $queries;
-		public $prepared;
+		public $query;
 		public $result;
 		
 		public static function setDefaults($host, $user, $pass, $database) {
@@ -25,34 +24,26 @@
 		public function connect() {
 			if (func_num_args() > 0) {
 				$args = func_get_args();
-				$this->connection = array(
-				"host"=>$args[0],
-				"user"=>$args[1],
-				"pass"=>$args[2],
-				"database"=>$args[3]
-				);
+				$this->connection = array();
+				$this->connection["host"] = $args[0];
+				if(func_num_args() > 1) $this->connection["user"] = $args[1];
+				if(func_num_args() > 2) $this->connection["pass"] = $args[2];
+				if(func_num_args() > 3) $this->connection["database"] = $args[3];
 			}
-			else {
-				$this->connection = self::$defaults;
-			}
-			
+			else $this->connection = self::$defaults;
 			// Connect to the database
-			$this->connection["handle"] = @mysql_connect($this->connection["host"],$this->connection["user"],$this->connection["pass"]);
-			if(!isset($this->connection["handle"])) return Log::status(__CLASS__, __FUNCTION__, FALSE, "Level 1 (Database Connection) Error: " . mysql_error(), array("query"=>$this->query));
-			
-			$this->connection["database"] = @mysql_select_db($this->connection["database"], $this->connection["handle"]);
-			if(!isset($this->connection["database"])) return Log::status(__CLASS__, __FUNCTION__, FALSE, "Level 2 (Database Selection) Error: " . mysql_error(), array("query"=>$this->query));
+			$this->connection["handle"] = new mysqli($this->connection["host"], $this->connection["user"], $this->connection["pass"], $this->connection["database"]);
+			if($this->connection["handle"]->connect_errno) return Log::status(__CLASS__, __FUNCTION__, FALSE, "Level 1 (Database Connection) Error: " . $this->connection["handle"]->connect_error, array("connection"=>$this->connection));
+			else return Log::status(__CLASS__, __FUNCTION__, TRUE, "Level 1 (Database Connection) Successful.", array("handle"=>$this->connection["handle"]));
 		}
 		
 		public function disconnect() {
-			$this->connect();
-			@mysql_close($this->connection["handle"]);
+			@mysql_close($this->connection["handle"]->close());
 		}
 		
 		public function executeBatch() {
 			$args = func_get_args();
 			
-			$this->connect();
 			if(isset($args[0]) && !empty($args[0])) $this->query = $args[0];
 			if(isset($args[1]) && $args[1] == TRUE) {
 				$this->queries = preg_split("#(?<!(\\\\))\;[\n\s\t]*#", $this->query, -1, PREG_SPLIT_NO_EMPTY);
@@ -60,33 +51,34 @@
 				$i = 0;
 				while($this->queries[$i]) {
 					if(trim($this->queries[$i]) != '' && trim($this->queries[$i]) != ';') {
-						$id = $this->prepare($this->queries[$i]);
-						if(Log::$status[$id]["success"] == "TRUE") $id = $this->execute($this->prepared);
+						$id = $this->execute($this->queries[$i]);
 					}
 					$i++;
 				}
 			}
+			else $id = $this->execute($this->query);
+		return $id;
+		}
+		
+		public function execute() {
+			$args = func_get_args();
+			
+			if(isset($args[0]) && !empty($args[0])) $this->query = $args[0];
+			if(isset($args[1]) && !empty($args[1])) $format = $args[1];
+			else $format = MYSQLI_BOTH;
+			
+			$result = $this->connection["handle"]->query($this->query);
+			if($this->connection["handle"]->errno) return Log::status(__CLASS__, __FUNCTION__, FALSE, "Level 2 (Execute Query) Error:" . $this->connection["handle"]->error, array("query"=>$this->query));
 			else {
-				$id = $this->prepare($this->query);
-				if(Log::$status[$id]["success"] == "TRUE") $id = $this->execute($this->prepared);
+				$this->result = array();
+				if($result->num_rows > 0) {
+					while($newRow = $result->fetch_array($format)) {
+						if(!empty($newRow)) $this->result[] = $newRow;
+					}
+				}
+				return Log::status(__CLASS__, __FUNCTION__, TRUE, array("query"=>$this->query,"result"=>$this->result));
 			}
-		return $id;
-		}
-		
-		public function prepare($var) {
-			$this->query = $var;
-			$this->prepared = @mysql_query($this->query);
-			if(!isset($this->prepared)) $id = Log::status(__CLASS__, __FUNCTION__, FALSE, "Level 3 (Query Encode) Error: " . mysql_error(), array("query"=>$this->query));
-			else $id = Log::status(__CLASS__, __FUNCTION__, TRUE, array("query"=>$this->query));
-		return $id;
-		}
-		
-		public function execute($var) {
-			$this->prepared = $var;
-			$this->result = @mysql_fetch_array($this->prepared);
-			if(!isset($this->prepared) || empty($this->prepared)) $id = Log::status(__CLASS__, __FUNCTION__, FALSE, "Level 4 (Fetch Query) Error: " . mysql_error(), array("query"=>$this->query));
-			else $id = Log::status(__CLASS__, __FUNCTION__, TRUE, array("query"=>$this->query));
-		return $id;
 		}
 	}
 ?>
+
