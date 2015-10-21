5847564d37be083c1748915b1e97b26e0467d01b
Jonathan Lawton <jlawton2@users.noreply.github.com>
2015-10-21 14:36:10 -0400

    Update session.php

--- session/session.php
+++ session/session.php
@@ -11,14 +11,18 @@
 			$args = func_get_args();
 			$args[2] = time() + $args[2] * 60;
 			if(!isset($args[3])) $args[3] = "/";
-			setcookie($args[0], $args[1], $args[2], $args[3]);
-			return Log::status(__CLASS__, __FUNCTION__, TRUE, array("cookie"=>$_COOKIE));
+			if(!isset($args[4])) $args[4] = (isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']);
+			if(!isset($args[5])) $args[5] = 0;
+			setcookie($args[0], $args[1], $args[2], $args[3], $args[4], $args[5]);
+			return Log::status(__CLASS__, __FUNCTION__, TRUE, array("args"=>$args, "cookie"=>$_COOKIE[$args[0]]));
 		}
 		
 		public static function unsetCookies() {
 			$args = func_get_args();
 			if(!isset($args[1])) $args[1] = "/";
-			setcookie($args[0], '', 1, $args[1]);
+			if(!isset($args[2])) $args[2] = (isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']);
+			if(!isset($args[3])) $args[3] = 0;
+			setcookie($args[0], '', 1, $args[1], $args[2], $args[3]);
 			unset($_COOKIE[$args[0]]);
 			return Log::status(__CLASS__, __FUNCTION__, TRUE);
 		}
